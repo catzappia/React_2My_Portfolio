7c9969b1162c73ace38e337afe674d6f80ae6921
Catherine Zappia <catzappia@aol.com>
2024-10-24 17:12:15 -0400

    fixing the bug

--- react_portfolio/src/App.jsx
+++ react_portfolio/src/App.jsx
@@ -1,12 +1,12 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Home from './pages/Home/Home'
-import Resume from './pages/Resume/Resume'
-import Portfolio from './pages/Portfolio/Portfolio'
-import Contact from './pages/Contact/Contact'
-import Footer from './components/Footer/Footer.jsx'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Resume from './pages/Resume/Resume';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Contact from './pages/Contact/Contact';
+import Footer from './components/Footer/Footer.jsx';
+import './App.css';
 
 function App() {
 
